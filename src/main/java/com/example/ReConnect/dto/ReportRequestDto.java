2990eb1fb513dc7b3f896f92455250d9c25ffa11
govdl <guss1301@gmail.com>
2025-07-24 20:27:06 +0900

    보고서 DB 수정

--- src/main/java/com/example/ReConnect/dto/ReportRequestDto.java
+++ src/main/java/com/example/ReConnect/dto/ReportRequestDto.java
@@ -11,6 +11,32 @@ public class ReportRequestDto {
     private String date;
     private String inputText;
 
+    @JsonProperty("report_title")
+    private String reportTitle;
+
+    @JsonProperty("key_emotions")
+    private JsonNode keyEmotions;
+
+    @JsonProperty("core_keywords")
+    private JsonNode coreKeywords;
+
+    @JsonProperty("indicator_trends")
+    private JsonNode indicatorTrends;
+
+    @JsonProperty("current_analysis")
+    private JsonNode currentAnalysis;
+
+    @JsonProperty("solution")
+    private JsonNode solution;
+
+    @JsonProperty("feedback_and_cheer")
+    private String feedbackAndCheer;
+
+    @JsonProperty("repetitive_pattern")
+    private String repetitivePattern;
+
+    private String recommendation;
+
     @JsonProperty("attachment_theory")
     private JsonNode attachmentTheory;
 
@@ -50,6 +76,33 @@ public class ReportRequestDto {
     public void setInputText(String inputText) {
         this.inputText = inputText;
     }
+
+    public String getReportTitle() { return reportTitle; }
+    public void setReportTitle(String reportTitle) { this.reportTitle = reportTitle; }
+
+    public JsonNode getKeyEmotions() { return keyEmotions; }
+    public void setKeyEmotions(JsonNode keyEmotions) { this.keyEmotions = keyEmotions; }
+
+    public JsonNode getCoreKeywords() { return coreKeywords; }
+    public void setCoreKeywords(JsonNode coreKeywords) { this.coreKeywords = coreKeywords; }
+
+    public JsonNode getIndicatorTrends() { return indicatorTrends; }
+    public void setIndicatorTrends(JsonNode indicatorTrends) { this.indicatorTrends = indicatorTrends; }
+
+    public JsonNode getCurrentAnalysis() { return currentAnalysis; }
+    public void setCurrentAnalysis(JsonNode currentAnalysis) { this.currentAnalysis = currentAnalysis; }
+
+    public JsonNode getSolution() { return solution; }
+    public void setSolution(JsonNode solution) { this.solution = solution; }
+
+    public String getFeedbackAndCheer() { return feedbackAndCheer; }
+    public void setFeedbackAndCheer(String feedbackAndCheer) { this.feedbackAndCheer = feedbackAndCheer; }
+
+    public String getRepetitivePattern() { return repetitivePattern; }
+    public void setRepetitivePattern(String repetitivePattern) { this.repetitivePattern = repetitivePattern; }
+
+    public String getRecommendation() { return recommendation; }
+    public void setRecommendation(String recommendation) { this.recommendation = recommendation; }
 
     public JsonNode getAttachmentTheory() { return attachmentTheory; }
     public void setAttachmentTheory(JsonNode attachmentTheory) { this.attachmentTheory = attachmentTheory; }
