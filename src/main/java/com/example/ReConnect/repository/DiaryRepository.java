eb85e327d34b0ae7bdaa91f8316dcd392e96eef3
cloky0000 <lyjin2002@gmail.com>
2025-10-02 09:18:02 +0900

    보고서랑 일기 가장 최근것 불러오기 엔드포인트 추가

--- src/main/java/com/example/ReConnect/repository/DiaryRepository.java
+++ src/main/java/com/example/ReConnect/repository/DiaryRepository.java
@@ -1,13 +1,27 @@
 package com.example.ReConnect.repository;
 
 import com.example.ReConnect.entity.Diary;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
+
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
     Optional<Diary> findByUserIdAndCoupleCodeAndQuestionNumber(String userId, String coupleCode, Integer questionNumber);
+    List<Diary> findByCoupleCodeAndQuestionNumber(String coupleCode, Integer questionNumber);
+    @Query("""
+            select d.questionNumber
+            from Diary d
+            where d.coupleCode = :code
+            group by d.questionNumber
+            having count(d) >= 2
+            order by d.questionNumber desc
+            """)
+    List<Integer> findCompletedQuestionNumbers(@Param("code") String coupleCode, Pageable pageable);
 }
